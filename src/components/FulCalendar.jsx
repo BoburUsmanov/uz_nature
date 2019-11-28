8441d79b104d923bdc5b6f1382897ed8f51cf7e4
BoburUsmanov <boburusmanov1994@gmail.com>
2019-11-28 10:37:17 +0500

    last

--- src/components/FulCalendar.jsx
+++ src/components/FulCalendar.jsx
@@ -2,31 +2,46 @@ import React, { Component } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import "./../css/calendar.css";
+import { connect } from "react-redux";
+import { events } from "./../redux/actions/actions";
 
-export default class FulCalendar extends Component {
+ class FulCalendar extends Component {
     constructor() {
         super();
-    
-
         this.toggle = this.toggle.bind(this);
     };
+    
+    componentDidMount() {
+     this.props.events();
+    }
 
     toggle(info) {
-        info.preventDefault();
-       alert(info.event.title)
+        // info.preventDefault();
     }
   
   render() {
+
+    let event_arr = this.props.eventss.filter(n => {
+      return (
+        n.lang === this.props.lang
+      );
+    }); 
+
+    let obj = {title:'',date:'',url:''};
+    let arr = [];
+    for(let i=0; i<event_arr.length; i++){
+      obj.title = event_arr[i].name;
+      obj.date = `${event_arr[i].c_y}-${event_arr[i].c_m}-${event_arr[i].c_d}`;
+      obj.url = `/events/${event_arr[i].one_id}`
+      arr.push(obj);
+      obj = {title:'',date:'',url:''}
+    }
     return (
       <div>
         <FullCalendar
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin]}
-          events={[
-            { title: "В СУРХАНДАРЬИНСКОЙ ОБЛАСТИ ПЛАНИРУЕТСЯ ПОСАДИТЬ 1 МЛН САЖЕНЦЕВ РАЗЛИЧНЫХ ДЕРЕВЬЕВ", date: "2019-11-19",url: '/news/5' },
-            { title: "АНДИЖАНСКАЯ ОБЛАСТЬ: ПРЕСЕЧЕНА НЕЗАКОННАЯ ДОБЫЧА ПЕСЧАНО-ГРАВИЙНОЙ СМЕСИ", date: "2019-10-27",url: '/news/6' },
-            { title: "В ТАШКЕНТЕ ПРОШЛА АКЦИЯ «ПОСАДИМ ДЕРЕВЬЯ ВМЕСТЕ!»", date: "2019-11-24",url: '/news/7' }
-          ]}
+          events = {arr}
           eventClick={this.toggle}
           locale = 'ru'
         />
@@ -34,3 +49,9 @@ export default class FulCalendar extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  lang: state.lang.lang,
+  eventss: state.events.events
+});
+export default connect(mapStateToProps, { events })(FulCalendar);
